d8de4012d027cce31eee5b650bef0896d05f223f
Saman Sandhu <ss25sand@edu.uwaterloo.ca>
2019-02-07 19:27:58 -0500

    Added Procfile and build client-side

---
+++ client/build/precache-manifest.9dba7ecb92daee88c14e47b50aae7a28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "05c95a5015c1f1709fb299f06c5477aa",
+    "url": "/static/media/pic1.05c95a50.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "105cbccbd595f1a7be98",
+    "url": "/static/js/main.105cbccb.chunk.js"
+  },
+  {
+    "revision": "1eeaf710069fe0934ece",
+    "url": "/static/js/1.1eeaf710.chunk.js"
+  },
+  {
+    "revision": "105cbccbd595f1a7be98",
+    "url": "/static/css/main.92a39209.chunk.css"
+  },
+  {
+    "revision": "f27113da313fbe23dea7f105ca14ada6",
+    "url": "/index.html"
+  }
+];
